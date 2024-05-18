70022f42a97b28d4301db4bad4d93e13e528db82
danielqueirozc <danielqueiroz.job@gmail.com>
2024-05-18 13:19:53 -0300

    updated responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { SearchInput } from "./searchInput";
 export function Header() {
     return (
         <header className="w-full bg-[#fff159] fixed m-auto p-5 z-10">
-            <div className="max-w-[1440px] flex justify-between items-center m-auto">
+            <div className="max-w-[1440px] flex justify-between gap-2 items-center m-auto">
                 <SearchInput />
                 <CartButton />
             </div>
